3372f6f892f666cc6a8afcbef00caec8efc54dba
Jericho Varde <vardejericho@gmail.com>
2026-08-23 12:58:37 +0800

    fix: scroll animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { HorizontalScroller } from '@/components/HorizontalScroller';
 
 export default function Home() {
   return (
-    <div className="min-h-screen relative overflow-hidden flex flex-col font-inter bg-background text-primary-dark">
+    <div className="min-h-screen relative overflow-x-hidden flex flex-col font-inter bg-background text-primary-dark">
       {/* Background Elements */}
       <div className="absolute -top-[20%] -right-[10%] w-[1000px] h-[1000px] rounded-full opacity-90 z-0 bg-[radial-gradient(circle,#E1C1B1_0%,rgba(246,237,229,0)_70%)]"></div>
       <div className="absolute top-[15%] -right-[10%] w-[800px] h-[800px] rounded-full border border-white z-0 opacity-80"></div>
